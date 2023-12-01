204e05a1434843427837d78b6fe00d463e14403f
swapnil1726@gmail.com <swapnil1726@gmail.com>
2023-12-01 19:03:11 +0530

    custom Error handling implemented

--- Talk/src/main/java/com/talk/configurations/SecurityConfig.java
+++ Talk/src/main/java/com/talk/configurations/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -35,6 +36,13 @@ public class SecurityConfig {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
 		http.csrf(c->c.disable())
+		
+		 .sessionManagement(session -> session
+	                .sessionFixation().migrateSession()
+	                .maximumSessions(2)
+	                    .maxSessionsPreventsLogin(true)
+	                    .expiredUrl("/login?expired")
+	            )
 		
 		 .authorizeHttpRequests(request -> request.requestMatchers("/admin-page")
 				 .hasAuthority("ADMIN").requestMatchers("/user-page").hasAuthority("USER")
