878edba7a41399b0a9ac302010c1eb68fe38649a
swapnil1726@gmail.com <swapnil1726@gmail.com>
2023-12-01 17:04:49 +0530

    global chat function implemetned

--- Talk/src/main/java/com/talk/controller/UserController.java
+++ Talk/src/main/java/com/talk/controller/UserController.java
@@ -4,6 +4,10 @@ import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.talk.dto.UserDto;
+import com.talk.model.ChatMessage;
 import com.talk.model.User;
 import com.talk.service.UserService;
 
@@ -70,5 +75,21 @@ public class UserController {
 	public String chatpage() {
 		return "chat";
 	}
+	
+	//websocket
+	  @MessageMapping("/chat.sendMessage")
+	    @SendTo("/topic/public")
+	    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+	        return chatMessage;
+	    }
+
+	    @MessageMapping("/chat.addUser")
+	    @SendTo("/topic/public")
+	    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+	                               SimpMessageHeaderAccessor headerAccessor) {
+	        // Add username in web socket session
+	        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+	        return chatMessage;
+	    }
 	
 }
